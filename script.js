69d9e3e4e7096601692b4bd0fe728f35a1d4ad3b
Dextersroom <naytiiey02@gmail.com>
2026-01-12 16:26:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Simple password prompt to protect portfolio
+const password = "viewonly123";
+const userInput = prompt("Enter password to view portfolio:");
+
+if (userInput !== password) {
+  document.body.innerHTML = "<h2 style='text-align:center;margin-top:50px;color:red;'>Access Denied 🚫</h2>";
+} 
+// If correct, page loads normally
+
 
